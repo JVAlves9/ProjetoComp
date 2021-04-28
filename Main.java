6b0094936addfb2ee8a83a304ab668932f95164a
Thiago Porfirio <>
2021-04-28 16:54:06 +0000

    Fixed avalieDls

--- Main.java
+++ Main.java
@@ -7,25 +7,22 @@ import java.nio.file.NoSuchFileException;
 
 class Main {
   public static void main(String[] args) throws Exception {
-    CharStream src = CharStreams.fromString("from config.ini in device_1 device_2");
+    CharStream src = CharStreams.fromString("from config.ini in device_1 load ip pass");
     DLSLexer lexer = new DLSLexer(src);
     TokenStream tkStream = new CommonTokenStream(lexer);
     DLSParser parser = new DLSParser(tkStream);
     DLSParser.StartContext result = parser.start();
     DLSParser.LoadfileContext lf = result.loadfile();
     
-    //ParseTree t1 = lf.load().getChild(2);
-    //System.out.println("Arvore: "+ t1.getText());
-   
    // Na linha de baixo ele cria o hashmap pra armazenar e chama a função pra armazenar de acordo com o comando
-   HashMap<String,HashMap> arqui = avalieDls(lf);
-    System.out.println(arqui);
+   
 
     if(parser.getNumberOfSyntaxErrors()>0) {
       System.out.println("entrada errada");
       return;
 	  }
-
+    HashMap<String,HashMap> arqui = avalieDls(lf);
+    System.out.println(arqui);
 
     ParseTree com = ConfigParser.getParser("config.ini");
 
@@ -45,19 +42,42 @@ class Main {
     if (exp.load() instanceof DLSParser.LoadWithOptionsContext) {
       
      ParseTree com = ConfigParser.getParser(exp.getChild(2).getText());
-      String configs[]={exp.load().getChild(6).getText()};
+      int nConfig = tamReal(exp.load().getChild(6).getChildCount());
+      String configs[] = new String[nConfig];
+      int k =0;
+    for(int i = 0; i <= nConfig-1; i++){
+      configs[i] = exp.load().getChild(6).getChild(i+k).getText();
+      k=k+1;
+        }
+
       r= ConfigParser.avalieConfig(ConfigParser.findSession(com, exp.load().getChild(2).getText()), configs); 
       return r;
+
     }
     if (exp.load() instanceof DLSParser.LoadSpecificConfigsContext) {
       
       ParseTree com = ConfigParser.getParser(exp.getChild(2).getText());
-      String sessions[] = {exp.load().getChild(2).getText()};
-      r= ConfigParser.avalie(com, sessions);
+      int nSession = tamReal(exp.load().getChild(2).getChildCount());
+      String sessions[] = new String[nSession];
+    int k =0;
+    for(int i = 0; i <= nSession-1; i++){
+      sessions[i] = exp.load().getChild(2).getChild(i+k).getText();
+      k=k+1;
+        }
+    r= ConfigParser.avalie(com, sessions);
       return r;
     }
-    System.out.println("NÃO ESTÁ NA GRAMATICA");
+    System.out.println("GRAMÁTICA ERRADA");
   return r;
+  }
+
+static int tamReal(int tamf){
+  int k=0;
+  int j=1;
+  while(j<tamf){
+    j=j+2;
+    k=k+1;}
+  return j-k;
   }
 
 }
